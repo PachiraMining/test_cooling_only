0694f4c6605c2730538bd929fecd7abeb9429ac1
minh <minh@dracaena.io>
2026-05-18 21:11:08 -0500

    add generate_pass.sh

---
+++ vecno_os/generate_pass_bit.sh
@@ -0,0 +1,28 @@
+md5sum e307c_v1.bit > e307c_v1.bit.md5sum 
+md5sum e307_v1.bit  > e307_v1.bit.md5sum
+md5sum e309c_v2.bit > e309c_v2.bit.md5sum
+md5sum e309_v4.bit  > e309_v4.bit.md5sum
+md5sum e335c_v2.bit > e335c_v2.bit.md5sum
+md5sum e335_v2.bit  > e335_v2.bit.md5sum
+md5sum e333_v1.bit  > e333_v1.bit.md5sum 
+md5sum e333c_v1.bit > e333c_v1.bit.md5sum
+md5sum e313_v1.bit  > e313_v1.bit.md5sum
+md5sum e311_v1.bit  > e311_v1.bit.md5sum
+md5sum e311c_v1.bit > e311c_v1.bit.md5sum
+
+#zip -P osprey123 g07c_testcool2.zip e307c_v1.bit e307c_v1.bit.md5sum
+#zip -P osprey123 g07_testcool2.zip  e307_v1.bit  e307_v1.bit.md5sum
+
+#zip -P osprey123 g09c_testcool3.zip e309c_v1.bit e309c_v1.bit.md5sum
+zip -P osprey123 e309_v4.zip  e309_v4.bit  e309_v4.bit.md5sum
+
+# zip -P osprey123 e335c.zip e335c_v2.bit e335c_v2.bit.md5sum
+# zip -P osprey123 e335.zip  e335_v2.bit  e335_v2.bit.md5sum
+
+#zip -P osprey123 g33_testcool2.zip  e333_v1.bit  e333_v1.bit.md5sum
+#zip -P osprey123 g33c_testcool2.zip e333c_v1.bit e333c_v1.bit.md5sum
+
+#zip -P osprey123 g13_testcool2.zip  e313_v1.bit  e313_v1.bit.md5sum
+#zip -P osprey123 g11_testcool.zip   e311_v1.bit  e311_v1.bit.md5sum
+#zip -P osprey123 g11c_testcool.zip  e311c_v1.bit e311c_v1.bit.md5sum
+
